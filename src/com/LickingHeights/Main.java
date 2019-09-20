9e8d4b31c9a8245377d85c6dd84c1e9c5b63cfd4
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-09-20 09:53:43 -0400

    userHeight

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,41 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+       String userHeight;
+        Scanner keyBoard;
+
+        keyBoard = new Scanner(System.in);
+
+
+
+
+    System.out.println("Hello, lets find out your BMI");
+    System.out.println("What is  your height in inches?");
+    userHeight = keyBoard.nextLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
