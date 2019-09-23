36b34dc1f1ea10794e0775ba0323abc62e19860d
Camron Caldwell <camron.caldwell.2022@student.lhschools.org>
2019-09-23 09:47:12 -0400

    double
    The double for userWeight and userHeight

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -9,16 +9,32 @@ public class Main {
 
 
 
+        double height,weight;
+        String userHeight;
+        String userWeight;
+
+
         Scanner keyBoard;
+
+
 
         keyBoard = new Scanner(System.in);
 
 
 
 
+
     System.out.println("Hello, lets find out your BMI");
-    System.out.println("What is  your height in feet?");
-    userHeight = keyBoard.
+    System.out.println("What is  your height in feet and inches?");
+    userHeight = keyBoard.nextLine();
+
+    System.out.println("Alright, Now what is your weight in lb?");
+    userWeight = keyBoard.nextLine();
+
+    System.out.println("Alright so your BMI is...");
+
+
+
 
 
 
